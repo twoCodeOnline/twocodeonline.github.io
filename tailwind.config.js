753e58013c2c354694b943a5682cd2304b4fcc3a
Hamza <hnasheralneam@gmail.com>
2024-07-19 18:57:46 -0400

    Tailwind works
    Co-authored-by: ehaansair <esair2515@users.noreply.github.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-   content: ["*.*"],
+   content: ["*.*", "./*.html", "./index.html", "./*/*.html", "./**/*.{html,js}"],
    theme: {
       extend: {},
    },
